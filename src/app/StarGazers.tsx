fe4f025629258e80e78ff9a1cd7f1c38b744c4ff
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-27 13:38:30 +0000

    add api call

---
+++ src/app/StarGazers.tsx
@@ -0,0 +1,14 @@
+//https://api.github.com/repos/vercel/next.js
+
+export default async function StarGazers() {
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const data = await res.json();
+
+  console.log(res.json);
+
+  return (
+    <div>
+      <p>{data.stargazers_count} people gazing at Next.js</p>
+    </div>
+  );
+}
